8b7c354e1e36a95f856e96f703a35906ab09f14e
Paulo de Lima Cavalcanti <paulolimac@gmail.com>
2012-05-08 12:49:53 -0300

    Commit de teste
    Signed-off-by: Paulo de Lima Cavalcanti <paulolimac@gmail.com>

---
+++ MeuHelloWorld/src/TestaMeuHelloWorld.java
@@ -0,0 +1,12 @@
+/*
+ * Projeto para teste do git
+ */
+public class TestaMeuHelloWorld {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Meu Hello World: Olá Mundo!!! :)");
+
+	}
+
+}
